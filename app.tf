29291037a6c44eb1d78557923a97cd1fe295037a
Sachin Hamirwasia <10850726+asksac@users.noreply.github.com>
2022-04-24 23:11:15 -0500

    Updated user data to download CA cert on launch

--- app.tf
+++ app.tf
@@ -43,6 +43,8 @@ resource "aws_instance" "client_app" {
 echo -e "[mongodb-org-4.0] \nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/amazon/2013.03/mongodb-org/4.0/x86_64/\ngpgcheck=1 \nenabled=1 \ngpgkey=https://www.mongodb.org/static/pgp/server-4.0.asc" | tee /etc/yum.repos.d/mongodb-org-4.0.repo
 yum install -y mongodb-org-shell
 
+wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem -P /home/ec2-user/
+
 EOF
 
   tags                    = merge(local.common_tags, tomap({"Name": "${var.app_shortcode}-client-app"}))
